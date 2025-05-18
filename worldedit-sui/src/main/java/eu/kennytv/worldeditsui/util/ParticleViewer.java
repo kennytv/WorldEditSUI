d40b43c09b1a7d5f93d37b1f0a7af8d76b5d3dec
darkvodou <facebookger@live.fr>
2025-05-18 19:35:09 +0200

    Add backward compatibility for spawnParticle method

--- worldedit-sui/src/main/java/eu/kennytv/worldeditsui/util/ParticleViewer.java
+++ worldedit-sui/src/main/java/eu/kennytv/worldeditsui/util/ParticleViewer.java
@@ -1,9 +1,13 @@
 package eu.kennytv.worldeditsui.util;
 
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 
 public final class ParticleViewer {
+
+    // The spawnParticle overload with the force parameter only exists on 1.20.6 and above
+    private static final boolean FORCE_AVAILABLE = hasForceParameter();
 
     private final Player player;
     private final Location location;
@@ -16,10 +20,24 @@ public final class ParticleViewer {
     }
 
     public void play(final ParticleData particle, final Location location) {
-        if (this.location.distanceSquared(location) <= particle.radiusSquared()) {
+        if (this.location.distanceSquared(location) > particle.radiusSquared()) {
+            return;
+        }
+
+        if (FORCE_AVAILABLE) {
             // Force the particle, so that the client doesn't discard it beyond 32 blocks
-            player.spawnParticle(this.particle.getParticle(), location, 1,
-                particle.offX(), particle.offY(), particle.offZ(), particle.speed(), this.particle.getData(), true);
+            player.spawnParticle(this.particle.getParticle(), location, 1, particle.offX(), particle.offY(), particle.offZ(), particle.speed(), this.particle.getData(), true);
+        } else {
+            player.spawnParticle(this.particle.getParticle(), location, 1, particle.offX(), particle.offY(), particle.offZ(), particle.speed(), this.particle.getData());
+        }
+    }
+
+    private static boolean hasForceParameter() {
+        try {
+            Player.class.getMethod("spawnParticle", Particle.class, Location.class, int.class, double.class, double.class, double.class, double.class, Object.class, boolean.class);
+            return true;
+        } catch (final NoSuchMethodException e) {
+            return false;
         }
     }
 }
